11f875ebe405fe77a28c79731c86f398eb4966bf
Dat Do <dtdat201202@gmail.com>
2024-01-28 13:14:03 +0700

    Cai dat handler findOne

--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -37,6 +37,14 @@ class ContactService {
             name: { $regex: new RegExp(name), $options: "i" },
         });
     }
+
+    async findById(id) {
+        return await this.Contact.findOne({
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        });
+    }
 }
+
+
 
 module.exports = ContactService;
